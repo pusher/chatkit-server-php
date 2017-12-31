ba7a7dfb5272a95bf4b8dc8f846d46874ac00f60
Neo Ighodaro <neo@creativitykills.co>
2017-12-31 19:25:56 +0100

    Added create room function

--- src/Chatkit.php
+++ src/Chatkit.php
@@ -222,6 +222,46 @@ class Chatkit
         return $response;
     }
 
+    /**
+     * Creates a new room.
+     *
+     * @param array $options  The room options
+     *                          [Available Options]
+     *                          • name (string|optional): Represents the name with which the room is identified.
+     *                              A room name must not be longer than 40 characters and can only contain lowercase letters,
+     *                              numbers, underscores and hyphens.
+     *                          • private (boolean|optional): Indicates if a room should be private or public. Private by default.
+     *                          • user_ids (array|optional): If you wish to add users to the room at the point of creation,
+     *                              you may provide their user IDs.
+     * @return array
+     */
+    public function create_room(array $options = [])
+    {
+        $body = [];
+
+        if (isset($options['name'])) {
+            $body['name'] = (string) $options['name'];
+        }
+
+        if (isset($options['private'])) {
+            $body['private'] = (bool) $options['private'];
+        }
+
+        if (isset($options['user_ids'])) {
+            $body['user_ids'] = (array) $options['user_ids'];
+        }
+
+        $ch = $this->create_curl(
+            $this->api_settings,
+            '/rooms',
+            $this->get_server_token(),
+            'POST',
+            $body
+        );
+
+        return $this->exec_curl($ch);
+    }
+
     public function send_message($id, $room_id, $text)
     {
         $body = array(
