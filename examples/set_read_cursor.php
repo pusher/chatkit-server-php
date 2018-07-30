b982fd8f05c6edd67988047fea44365c0ca9969f
Hamilton Chapman <hamchapman@gmail.com>
2018-07-30 09:53:06 +0100

    Add example for setReadCursor

---
+++ examples/set_read_cursor.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../vendor/autoload.php');
+
+$chatkit = new Chatkit\Chatkit([
+  'instance_locator' => 'your:instance:locator',
+  'key' => 'your:key'
+]);
+
+print_r($chatkit->setReadCursor([
+  'user_id' =>  'ham',
+  'room_id' =>  456789,
+  'position' =>  123,
+]));
