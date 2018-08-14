7ee1e17c53a63bccc830efbf37bfea8bf060f044
Hamilton Chapman <hamchapman@gmail.com>
2018-08-14 11:38:51 +0100

    Add joinRoom example

---
+++ examples/join_room.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../vendor/autoload.php');
+
+$chatkit = new Chatkit\Chatkit([
+  'instance_locator' => 'your:instance:locator',
+  'key' => 'your:key'
+]);
+
+print_r($chatkit->joinRoom([
+  'user_id' => 'ham',
+  'room_id' => 123
+]));
